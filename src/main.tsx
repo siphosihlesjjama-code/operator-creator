bbdf216275d8e86159055a821ce0103ebcaae294
siphosihlesjjama-code <siphosihle.sj.jama@gmail.com>
2026-09-18 21:04:15 +0200

    chore: add React entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,1 @@
+import React from "react"; import {createRoot} from "react-dom/client"; import "./styles.css"; import HomePage from "./HomePage"; createRoot(document.getElementById("root")!).render(<React.StrictMode><HomePage/></React.StrictMode>);
